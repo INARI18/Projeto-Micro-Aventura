541cd952c36f8216d9907bfbe9e4ff5903cdd3ef
Pedro Vitor <pvlo2004@gmail.com>
2024-04-27 15:09:57 -0300

    Correção do código
    -Correção de lógica(agora compila)
    -Correção da mensagem original
    -Renomeação das variáveis

--- cacada_selvagem_1.py
+++ cacada_selvagem_1.py
@@ -1,45 +1,47 @@
-#Cifra_de_Vigenère
+#Correcao da mensagem original:
 
 """
 AFVFYFNE KO NQPVSZ MIQOFQN
-LVGOFFZ WF JDW-AJMIVOZ
-EMFVFZSE WKH SHUSW MRUUUKVVZ
+LVGOFFZ WFJDW AJMIUOZ
+EMFVFZSE WKHSHUSW MRUUUKVVZ
 GDXYJWGHHKE
 JC GUVYE BOCQVZ 48 LZXZG
-DAQ ZBGHD-VO LAN YBJFUK
+DAQZBGHD-VO LAN YBJFUK
 VM 5 VFFTVLG
 """
 
-LETRA_FINAL = ""
-j = -1 #indice que percorre a SENHA TOPAZIO
+#senha: TOPAZIO
 
 print("Qual a SENHA?")
-SENHA = "Topazio" #str(input()) #recebe a SENHA
-print("Digite cada linha da LETRA_FINAL secreta.")
+SENHA = str(input().upper())
+print("Digite cada linha da mensagem secreta.")
 print("Quando terminar, digite FIM.")
 print()
 
-MENSAGEM_CRIPTO = "INICIO"
+MENSAGEM_CRIPTO = ""
+MENSAGEM_FINAL = ""
+j = 0
 
 while MENSAGEM_CRIPTO != "FIM":
 	
 	MENSAGEM_CRIPTO = str(input().upper())
 	if MENSAGEM_CRIPTO == "FIM":
 		print("Execucao terminada!")
-		exit()
-
+		break
+	MENSAGEM_FINAL = ""
 	for i in range(0,len(MENSAGEM_CRIPTO)):
-		LETRA_CODIGO = MENSAGEM_CRIPTO[i]
-		if (LETRA_CODIGO < 'A' or LETRA_CODIGO > 'Z'):
-			LETRA_FINAL = LETRA_FINAL + LETRA_CODIGO
-		j = j + 1 #avança para o proximo indice da SENHA
-		if j > len(SENHA) - 1: #reinicia o indice da SENHA
-			j = 0
-		ASCII_SENHA = ord(SENHA[j]) - ord('A') + 1
-		ASCII_CODIGO = ord(LETRA_CODIGO) - ord('A') + 1
-		#print(ASCII_SENHA,ASCII_CODIGO)
-		if ASCII_SENHA <= ASCII_CODIGO:
-			ASCII_SENHA = ASCII_SENHA + 26
-		ASCII_LETRA_FINAL = ASCII_SENHA - ASCII_CODIGO
-		LETRA_FINAL = LETRA_FINAL + chr(ASCII_LETRA_FINAL + ord('A') - 1)
-	print(LETRA_FINAL)
+		LETRA_CRIPTO = MENSAGEM_CRIPTO[i]
+		if (LETRA_CRIPTO < 'A' or LETRA_CRIPTO > 'Z'):
+			MENSAGEM_FINAL = MENSAGEM_FINAL + LETRA_CRIPTO
+		else:
+			if j > len(SENHA) - 1:
+				j = 0
+			ASCII_SENHA = ord(SENHA[j]) - ord('A') + 1
+			ASCII_CRIPTO = ord(LETRA_CRIPTO) - ord('A') + 1
+			if ASCII_SENHA <= ASCII_CRIPTO:
+				ASCII_SENHA = ASCII_SENHA + 26
+			ASCII_LETRA_FINAL = ASCII_SENHA - ASCII_CRIPTO
+			MENSAGEM_FINAL = MENSAGEM_FINAL + chr(ASCII_LETRA_FINAL + ord('A') - 1)
+			j = j + 1
+	print(MENSAGEM_FINAL)
+	print()
