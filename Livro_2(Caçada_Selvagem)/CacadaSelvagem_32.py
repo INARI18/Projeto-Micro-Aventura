2737e2feef3e8b667386970376998a6304dbb5d2
Beatriz Machado <beatrizrolandmachado@gmail.com>
2024-05-29 21:38:11 -0300

    + piscadas

--- Livro_2(Caçada_Selvagem)/CacadaSelvagem_32.py
+++ Livro_2(Caçada_Selvagem)/CacadaSelvagem_32.py
@@ -7,53 +7,38 @@ DESENHO_ATUAL = ""
 
 stdscr = curses.initscr()
 curses.curs_set(0)
+piscadas = 9
 
-for i in range (3):
+for i in range(piscadas):
     stdscr.clear()
+    stdscr.refresh() 
+
+    for j in range(10):
+        # OLHO FECHADO
+        if i == 1 or i == 3 or i == 5 or i == 7:
+            stdscr.addstr(0, 1, CONTORNO_OLHO * 2)            # 12 (*)
+            stdscr.addstr(1, 0, CONTORNO_OLHO * 2 + "*" * 2)  # 14 (*)
+            stdscr.addstr(2, 0, CONTORNO_OLHO * 2 + "*" * 2)  # 14 (*)
+            stdscr.addstr(3, 1, CONTORNO_OLHO * 2)            # 12 (*)
+            stdscr.addstr(4, 2, CONTORNO_OLHO + "*" * 4)      # 10 (*)
+            stdscr.addstr(5, 4, CONTORNO_OLHO)                #  6 (*)
+        # OLHO ABERTO
+        else:
+            stdscr.addstr(0, 4, CONTORNO_OLHO)
+            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA[0:6] + CONTORNO_OLHO[0:2]
+            stdscr.addstr(1, 2, DESENHO_ATUAL)
+
+            linha = 2
+            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA + CONTORNO_OLHO[0:2]
+            while linha != 5:
+                stdscr.addstr(linha, 0, DESENHO_ATUAL)
+                linha += 1
+
+            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA[0:6] + CONTORNO_OLHO[0:2]
+            stdscr.addstr(5, 2, DESENHO_ATUAL)
+            stdscr.addstr(6, 4, CONTORNO_OLHO)
+
     stdscr.refresh()
-    for j in range (10):
-
-        if i == 0:
-            stdscr.addstr(0, 4, CONTORNO_OLHO)
-            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA[0:6] + CONTORNO_OLHO[0:2]
-            stdscr.addstr(1, 2, DESENHO_ATUAL)
-
-            linha = 2
-            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA + CONTORNO_OLHO[0:2]
-            while linha != 5:
-
-                stdscr.addstr(linha,0,DESENHO_ATUAL)
-                linha+=1
-
-            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA[0:6] + CONTORNO_OLHO[0:2]
-            stdscr.addstr(5, 2, DESENHO_ATUAL)
-            stdscr.addstr(6, 4, CONTORNO_OLHO)
-
-        if i == 1:
-            stdscr.addstr(0, 1, CONTORNO_OLHO*2) # 12 (*)
-            stdscr.addstr(1, 0, CONTORNO_OLHO*2 + "*"*2) # 14 (*)
-            stdscr.addstr(2, 0, CONTORNO_OLHO*2 + "*"*2) # 14 (*)
-            stdscr.addstr(3, 1, CONTORNO_OLHO*2) # 12 (*)
-            stdscr.addstr(4, 2, CONTORNO_OLHO + "*"*4) # 10 (*)
-            stdscr.addstr(5, 4, CONTORNO_OLHO) # 6 (*)
-        
-        if i == 2:
-            stdscr.addstr(0, 4, CONTORNO_OLHO)
-            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA[0:6] + CONTORNO_OLHO[0:2]
-            stdscr.addstr(1, 2, DESENHO_ATUAL)
-
-            linha = 2
-            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA + CONTORNO_OLHO[0:2]
-            while linha != 5:
-
-                stdscr.addstr(linha,0,DESENHO_ATUAL)
-                linha+=1
-
-            DESENHO_ATUAL = CONTORNO_OLHO[0:2] + PUPILA[0:6] + CONTORNO_OLHO[0:2]
-            stdscr.addstr(5, 2, DESENHO_ATUAL)
-            stdscr.addstr(6, 4, CONTORNO_OLHO)
-
-        stdscr.refresh()
-        sleep(5)
+    sleep(2)
 
 curses.endwin()
