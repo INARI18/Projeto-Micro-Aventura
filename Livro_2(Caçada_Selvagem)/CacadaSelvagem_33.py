83337f2122dc55c4bba02389995e0d7ebd9b60f8
Beatriz Machado <beatrizrolandmachado@gmail.com>
2024-07-02 17:09:01 -0300

    update
    - implementada função para criar matriz

--- Livro_2(Caçada_Selvagem)/CacadaSelvagem_33.py
+++ Livro_2(Caçada_Selvagem)/CacadaSelvagem_33.py
@@ -6,4 +6,15 @@ def CriaTela(linhas, colunas):
 
     for i in range (linhas):
         linha = []
+        
+        for j in range (colunas):
+            linha.append(0)
+        
+        matriz.append(linha)
     
+    return matriz
+
+matriz = CriaTela(10, 10)
+
+for i in matriz:
+    print(i)
